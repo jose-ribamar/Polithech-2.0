56bff5ad3c6a145b9870f6aba1d3469f3bc4feee
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2024-05-28 23:17:34 -0300

    Código com erro

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 /*
 |--------------------------------------------------------------------------
 | Create The Application
+|
 |--------------------------------------------------------------------------
 |
 | The first thing we will do is create a new Laravel application instance
