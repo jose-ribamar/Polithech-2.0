14a05d7be23518741c1b22c8d9cd4ad14b3221cf
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2024-06-03 22:49:25 -0300

    testes de rota

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index(){
+        return 'index';
+    }
+
+    public function show($id=' '){
+        return "show".$id;
+    }
+};
