e629facb3714427916c496e80e95f33bbf0a8316
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2024-06-03 22:02:00 -0300

    Grupos em laravel

--- routes/web.php
+++ routes/web.php
@@ -18,33 +18,50 @@ Route::get('/', function () {
 
     // $user = User::Factory()->create();
     // auth()->login($user);
-    return view('welcome');
+    return redirect()->route('admin.cliente');
+});
+
+Route::name('admin.')->group(function(){
+    Route::get('admin/dashboard', function(){
+    return "dashboard";
+    })->name('dashboard');
+
+    Route::get('admin/user', function(){
+        return "user";
+    })->name('user');
+
+    Route::get('admin/cliente', function(){
+        return "cliente";
+    })->name('cliente');
 });
 
 
 
-Route::any('/any', function(){
-    return "Permite todo tipo de acesso http(put, delete, get, post)";
-});
 
-Route::match(['put', 'delet'],'/match',function(){
-    return "Permite apenas acessos definidos";
-});
-Route::get('/produto/{id}/{cat?}',function($id, $cat = 'Limpesa'){
-    return "O id do produto é: ".$id."<br>" . "A categoria é: ".$cat;
-});
 
-Route::redirect('/sobre', 'empresa');
+// Route::any('/any', function(){
+//     return "Permite todo tipo de acesso http(put, delete, get, post)";
+// });
 
-Route::view('/empresa','site/empresa');
+// Route::match(['put', 'delet'],'/match',function(){
+//     return "Permite apenas acessos definidos";
+// });
+// Route::get('/produto/{id}/{cat?}',function($id, $cat = 'Limpesa'){
+//     return "O id do produto é: ".$id."<br>" . "A categoria é: ".$cat;
+// });
 
-Route::get('/news',function(){
-    return view('news');
-})->name('noticias');
+// Route::redirect('/sobre', 'empresa');
 
-Route::get('/novidades', function(){
-    return redirect()->route('noticias');
-});
+// Route::view('/empresa','site/empresa');
+
+// Route::get('/news',function(){
+//     return view('news');
+// })->name('noticias');
+
+// Route::get('/novidades', function(){
+//     return redirect()->route('noticias');
+// });
+// 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
