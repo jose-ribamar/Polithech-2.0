c2de21730799fea2cd2e44c6922dab1d051799f9
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2024-06-07 23:41:58 -0300

    Corrigindo erro ao se criar a tabela, limites de tamanho

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Define o comprimento padrão das strings do esquema do Laravel
+        // para evitar erros ao criar índices únicos em campos longos.
+        Schema::defaultStringLength(191);
     }
 }
