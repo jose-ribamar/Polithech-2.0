f572e438863156a4f2fbad1f78fcd1ac7e28935e
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2024-06-09 23:17:44 -0300

    Modificação de arquivos testes

--- database/migrations/2024_06_08_022851_produtos.php
+++ database/migrations/2024_06_08_022851_produtos.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('nomee');
+            // $table->string('nome_completo');
             $table->timestamps();
         });
     }
