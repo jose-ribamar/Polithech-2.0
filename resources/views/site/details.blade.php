399b7cff080210aaee3872ed3cc33d896d9c8df8
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2024-06-20 23:00:32 -0300

    creation of description screens for products

---
+++ resources/views/site/details.blade.php
@@ -0,0 +1,23 @@
+@extends('site.layout')
+@section('title','Detalhes')
+@section('conteudo')
+
+<div class="row container">
+    <div class="col s12 m6 ">
+
+        <img src="{{$produto->imagem}}" class="responsive-img">
+        
+    </div>
+
+    <div class="col s12 m6">
+        <h1>{{$produto->nome}}</h1>
+        <p>Postato por {{$produto->descricao}}</p> 
+        <p>Postato por: {{$produto->user->firstName}}</p>  
+        <p>Categoria: {{$produto->categoria->nome}}</p>  
+        <button class="btn orange btn-la"> Comprar</button>
+    </div>
+
+</div>
+
+
+@endsection
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,25 +1,45 @@
 @extends('site.layout')
 
+@section('title', 'home')
+
 @section('conteudo')
 
 <div class="row container">
-  @foreach ($produto as $produtos)
+
+  @foreach ($produtos as $produto)
+
     <div class="col s12 m4">
+
         <div class="card">
+
             <div class="card-image">
-              <img src="{{$produtos->imagem}}">
-              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+
+              <img src="{{ $produto->imagem }}">
+
+              <a href="{{ route('site.details', $produto->slug) }}" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">remove_red_eye</i></a>
+
             </div>
+
             <div class="card-content">
-              <span class="card-title">{{ Str::limit($produtos->nome, 20) }}</span>
-              <p>{{ Str::limit($produtos->descricao, 20) }}</p>
+
+              <span class="card-title">{{ Str::limit($produto->nome, 20) }}</span>
+
+              <p>{{ Str::limit($produto->descricao, 20) }}</p>
+
             </div>
+
           </div>
+
     </div>
+
   @endforeach
+
 </div>
+
 <div class="row center">
-  {{ $produto->links('custom.pagination') }}
+
+  {{ $produtos->links('custom.pagination') }}
+
 </div>
 
 @endsection
