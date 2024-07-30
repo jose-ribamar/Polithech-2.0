bb0a13200f57e807cd204ff9a443094335eae7cf
José ribamar Gomes de Sousa <107278033+jose-ribamar@users.noreply.github.com>
2024-07-29 23:07:58 -0300

    Adding the shopping cart option in the project

--- resources/views/site/details.blade.php
+++ resources/views/site/details.blade.php
@@ -14,8 +14,18 @@
         <p>Postato por {{$produto->descricao}}</p> 
         <p>Preço {{ number_format($produto->preco, 2, ',', '.')}} R$</p> 
         <p>Postato por: {{$produto->user->firstName}}</p>  
-        <p>Categoria: {{$produto->categoria->nome}}</p>  
+        <p>Categoria: {{$produto->categoria->nome}}</p> 
+        
+        <form action="{{ route('site.addcarrinho') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+
+            <input type="hidden" name="id" value="{{$produto->id}}">
+            <input type="hidden" name="name" value=" {{$produto->descricao}}">
+            <input type="hidden" name="price" value="{{ $produto->preco }} ">
+            <input type="number" name="quantity" value="1">
+            <input type="hidden" name="attributes" value="{{$produto->categoria->imagem}}">
         <button class="btn orange btn-la"> Comprar</button>
+    </form>
     </div>
 
 </div>
